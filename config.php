c6f3a7de2effec65f364fcffa218b8f5d4c79ae3
冷寂233 <1481169617@qq.com>
2015-09-29 21:49:03 +0800

    no commit message

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+/*数据库配置*/
+$host = "55e1b3404f0d7.gz.cdb.myqcloud.com"; //MYSQL主机
+$port = 3732; //MYSQL主机
+$user = "cdb_outerroot"; //MYSQL用户
+$pwd ="hj19981124"; //MYSQL密码
+$dbname = "52ysmz"; //数据库名
+
+/*系统配置*/
+$db_qz = "wjob"; //数据表前缀
+$cache = 0; //缓存方式(0为数据库1为文件)
+?>
